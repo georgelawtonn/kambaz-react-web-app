d1998fc6a93757aa4c5d5a8a83b26b5a86086d97
georgelawtonn <glawton03@outlook.com>
2025-01-12 17:21:50 -0500

    Fixed some possible build errors p2

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -4,7 +4,7 @@ export default function Profile() {
     return (
         <div id="wd-profile-screen">
             <h3>Profile</h3>
-            <input defaultValue="alice" placeholder="username" class="wd-username"/><br/>
+            <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
             <input defaultValue="123" placeholder="password" type="password"
                    className="wd-password"/><br/>
             <input defaultValue="Alice" placeholder="First Name" id="wd-firstname"/><br/>
