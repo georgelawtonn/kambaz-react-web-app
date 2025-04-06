d9e4b3782cb0faeb1beb5174cbe3d867ce90b98e
georgelawtonn <glawton03@outlook.com>
2025-04-06 06:38:24 -0400

    A6 Initial 2

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -8,7 +8,7 @@ import {FaPlus} from "react-icons/fa6";
 export default function Users() {
     const [users, setUsers] = useState<any[]>([]);
     const [role, setRole] = useState("");
-    const [name, setName] = useState("");
+    // const [name, setName] = useState("");
     const createUser = async () => {
         const user = await client.createUser({
             firstName: "New",
@@ -22,7 +22,7 @@ export default function Users() {
         setUsers([...users, user]);
     };
     const filterUsersByName = async (name: string) => {
-        setName(name);
+        // setName(name);
         if (name) {
             const users = await client.findUsersByPartialName(name);
             setUsers(users);
